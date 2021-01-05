c0ff21dbf84367fe1e220cca44dd30b8fac5f269
Kaofang <benyapa_kh@cmu.ac.th>
2021-01-05 15:41:44 +0700

    update_codepayloan

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
 using namespace std;
 
-int main(){	
+int main(){
+    double N,rate,jd ;
 	cout << "Enter initial loan: ";
+	cin >> N ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
+	cin>>jd ;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +18,25 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	double B ;
+	for(int count = 1;N>0;count++){
+	    	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << count; 
+	cout << setw(13) << left << N;
+	B = N*(rate/100);
+	cout << setw(13) << left << B ;
+	N = N+B;
+	cout << setw(13) << left << N ;
+	if(N<jd){ 
+	    jd=N;
+	}
+	cout << setw(13) << left << jd;
+	N = N-jd;
+	cout << setw(13) << left << N;
+	cout << "\n";
+	    
+	}
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
 	return 0;
 }
